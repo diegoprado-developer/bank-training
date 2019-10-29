110e8e68d86b8118d14e2e4cd2631d1b48f5094d
diegoprado-developer <diego.droiddeveloper@gmail.com>
2019-10-29 11:05:47 -0300

    instalacao e teste do angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { MatDialog } from '@angular/material';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bank-training';
+
+  constructor(public dialog: MatDialog){
+
+  }
+
+  openDialog(){
+    const dialogRef = this.dialog.open(AppComponent, {
+      height: '350px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog Result: ${result}`);
+    });
+
+  }
 }
