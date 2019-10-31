c4f39602053be1426bc08355d7fcf7588c856453
diegoprado-developer <diego.droiddeveloper@gmail.com>
2019-10-31 13:18:21 -0300

    estilizando tela de cadastro

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ContentComponent } from './../content/content.component'
 
+import { CadastroClientesComponent } from './../cadastro-clientes/cadastro-clientes.component';
+
 
 const routes: Routes = [
   { path: '', component: ContentComponent },
-  { path: 'cadastro', component: ContentComponent }
+  { path: 'cadastro-clientes', component: CadastroClientesComponent }
 ]
 
 
